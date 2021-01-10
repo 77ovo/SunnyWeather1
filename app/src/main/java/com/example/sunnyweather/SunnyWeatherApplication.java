68db97312eaeb90c626607e736666b33de2fd462
77ovo <1732105838@qq.com>
2021-01-10 19:56:07 +0800

    完成

--- app/src/main/java/com/example/sunnyweather/SunnyWeatherApplication.java
+++ app/src/main/java/com/example/sunnyweather/SunnyWeatherApplication.java
@@ -1,23 +1,18 @@
 package com.example.sunnyweather;
 
+import android.annotation.SuppressLint;
 import android.app.Application;
 import android.content.Context;
 
 public class SunnyWeatherApplication extends Application {
-    private static Context context;
 
+
+    public static  final String TOKEN = "p9KDMqkGvbGR35ex";
+    @SuppressLint("StaticFieldLeak")
+    static public Context context;
     @Override
     public void onCreate() {
         super.onCreate();
-        context = getApplicationContext();
+        context =getApplicationContext();
     }
-
-    /**
-     * 获取全局上下文*/
-    public static Context getContext() {
-        return context;
-    }
-    private static String Token="p9KDMqkGvbGR35ex";
-
 }
-
